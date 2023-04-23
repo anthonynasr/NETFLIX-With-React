b17c0dc24b82ffce71ed457ebfc35887a9b31f1a
Anthony Nasr <anthony.nasr@datategy.net>
2023-04-23 21:50:28 +0200

    Remove key

--- src/Requests.js
+++ src/Requests.js
@@ -1,4 +1,4 @@
-const key= 'd7d6becf0c602339a3811613573359a9';
+const key= '';
 const requests = {
     requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US`,
     requestTopRated :`https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US`,
